ee0e8a6bed4216b07e62c1be1c7442d201852bb4
semantic-release <semantic-release>
2023-08-30 11:58:09 +0000

    0.3.0
    Automatically generated by python-semantic-release

--- interstate75wrapper/__init__.py
+++ interstate75wrapper/__init__.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 from .datetime import DateTime
 from .display_type import DisplayType
