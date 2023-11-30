2b23191b4aa0023f4f5b67ce8c673eed46839dd9
Andrew Wilkinson <andrewjwilkinson@gmail.com>
2023-11-30 11:32:49 +0000

    feat: Add emulated sleep_ms function.

--- i75/emulated/machine.py
+++ i75/emulated/machine.py
@@ -14,3 +14,9 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import time
+
+import mp_time
+
+time.sleep_ms = mp_time.sleep_ms
--- i75/emulated/mp_time.py
+++ i75/emulated/mp_time.py
@@ -14,3 +14,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import time
+
+def sleep_ms(delay: int) -> None:
+    time.sleep(delay / 1000.0)
