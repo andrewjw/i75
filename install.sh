c7e87baad231d5873b9a7238798cafcdd3dcdb7d
Andrew Wilkinson <andrewjwilkinson@gmail.com>
2023-12-01 15:40:08 +0000

    chore: Add new timezone module to install script.

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 ampy ${@:1} mkdir i75 --exists-okay
+ampy ${@:1} mkdir i75/tz --exists-okay
 ampy ${@:1} put i75/__init__.py i75/__init__.py
 ampy ${@:1} put i75/basei75.py i75/basei75.py
 ampy ${@:1} put i75/colour.py i75/colour.py
 ampy ${@:1} put i75/datetime.py i75/datetime.py
 ampy ${@:1} put i75/graphics.py i75/graphics.py
 ampy ${@:1} put i75/nativei75.py i75/nativei75.py
+ampy ${@:1} put i75/tz/__init__.py i75/tz/__init__.py
+ampy ${@:1} put i75/tz/europe_london.py i75/tz/europe_london.py
