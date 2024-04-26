384dbf30585970da1870a9793c1d46d8119934f8
Andrew Wilkinson <andrewjwilkinson@gmail.com>
2024-04-26 21:28:57 +0100

    chore: Update to new stubs location.

--- code_style.sh
+++ code_style.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-MYPYPATH=./stubs:./i75/emulated:$MYPYPATH mypy bin/i75
+MYPYPATH=./i75/stubs:./i75/emulated:$MYPYPATH mypy bin/i75
 
-MYPYPATH=./stubs:./i75/emulated:$MYPYPATH mypy bin/i75-convert-image
+MYPYPATH=./i75/stubs:./i75/emulated:$MYPYPATH mypy bin/i75-convert-image
 
-MYPYPATH=./stubs:./i75/emulated:$MYPYPATH mypy -m i75
+MYPYPATH=./i75/stubs:./i75/emulated:$MYPYPATH mypy -m i75
 
-MYPYPATH=./stubs:./i75/emulated:$MYPYPATH mypy -p examples
+MYPYPATH=./i75/stubs:./i75/emulated:$MYPYPATH mypy -p examples
 
-MYPYPATH=./stubs:./i75/emulated:$MYPYPATH mypy -m tests
+MYPYPATH=./i75/stubs:./i75/emulated:$MYPYPATH mypy -m tests
 
 ${PYCODESTYLE:-pycodestyle} bin/ i75/ examples/ tests/
